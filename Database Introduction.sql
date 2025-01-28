61b2f826abdb7ace87fbe886f5354f27f59d0e3a
Ivaylo Minchev <159180386+ivaylominchev@users.noreply.github.com>
2025-01-28 22:59:45 +0200

    Exercise 19

--- Database Introduction.sql
+++ Database Introduction.sql
@@ -392,3 +392,11 @@ INSERT INTO [Occupancies]([DateOccupied], [AccountNumber], [RateApplied], [Phone
 	 VALUES (NULL, NULL, NULL, NULL, NULL),
 			( NULL, NULL, NULL, NULL, NULL),
 			(NULL, NULL, NULL, NULL, NULL)
+
+
+--Exercise 19
+USE SoftUni
+
+SELECT * FROM [Towns]
+SELECT * FROM [Departments]
+SELECT * FROM [Employees]
