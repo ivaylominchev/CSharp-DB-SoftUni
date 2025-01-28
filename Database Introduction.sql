a824b46c3b10ce155c76912af073a16c43b984b6
Ivaylo Minchev <159180386+ivaylominchev@users.noreply.github.com>
2025-01-28 19:33:15 +0200

    Problem 04

--- Database Introduction.sql
+++ Database Introduction.sql
@@ -5,14 +5,14 @@ CREATE DATABASE [Minions]
 USE Minions
 CREATE TABLE [Minions]
 (	
-	[Id] INT PRIMARY KEY IDENTITY(1, 1) NOT NULL,
+	[Id] INT PRIMARY KEY NOT NULL,
 	[Name] NVARCHAR(50) NOT NULL,
 	[Age] INT
 )
 
 CREATE TABLE [Towns]
 (
-	[Id] INT PRIMARY KEY IDENTITY NOT NULL,
+	[Id] INT PRIMARY KEY NOT NULL,
 	[Name] NVARCHAR(85) NOT NULL
 )
 
@@ -22,4 +22,17 @@ ADD [TownId] INT
 
 ALTER TABLE [Minions]
 ADD CONSTRAINT [FK_Minions_Towns_Id]
-FOREIGN KEY ([Id]) REFERENCES [Towns]([Id])
+FOREIGN KEY ([TownId]) REFERENCES [Towns]([Id])
+
+
+--Exercise 04
+INSERT INTO [Towns]([Id],[Name])
+	 VALUES (1, 'Sofia'),
+			(2, 'Plovdiv'),
+			(3, 'Varna')
+
+INSERT INTO [Minions]([Id], [Name], [Age], [TownId])
+	 VALUES (1, 'Kevin', 22, 1),
+			(2, 'Bob', 15, 3),
+			(3, 'Steward', NULL, 2)
+
