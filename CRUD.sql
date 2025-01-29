65ae4a05847d272514117687a02dc4bb0e112dfc
Ivaylo Minchev <159180386+ivaylominchev@users.noreply.github.com>
2025-01-29 13:16:48 +0200

    Update CRUD.sql

--- CRUD.sql
+++ CRUD.sql
@@ -91,3 +91,13 @@ CREATE
 		  FROM [Employees]
 	   )
 
+--Task 17
+CREATE
+  VIEW [V_EmployeeNameJobTitle]
+    AS (
+			SELECT CONCAT([FirstName], ' ', [MiddleName], ' ', [LastName]) 
+                AS [Full Name],
+	               [JobTitle]
+              FROM [Employees]
+	   )
+
