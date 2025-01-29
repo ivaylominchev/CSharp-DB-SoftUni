37488bc2fc239a062631733dc2ba6eb1e70c0eda
Ivaylo Minchev <159180386+ivaylominchev@users.noreply.github.com>
2025-01-30 00:44:36 +0200

    Update Table Relations.sql

--- Table Relations.sql
+++ Table Relations.sql
@@ -152,3 +152,38 @@ CREATE TABLE [OrderItems](
 	CONSTRAINT [PK_OrderItems] PRIMARY KEY([OrderID], [ItemID])
 )
 
+
+--Task 6
+CREATE DATABASE [University]
+USE [University]
+
+CREATE TABLE [Majors](
+	[MajorID] INT PRIMARY KEY NOT NULL,
+	[Name] VARCHAR(64) NOT NULL
+)
+
+CREATE TABLE [Students](
+	[StudentID] INT PRIMARY KEY NOT NULL,
+	[StudentNumber] INT NOT NULL,
+	[StudentName] VARCHAR(64) NOT NULL,
+	[MajorID] INT FOREIGN KEY REFERENCES [Majors]([MajorID])
+)
+
+CREATE TABLE [Payments](
+	[PaymentID] INT PRIMARY KEY NOT NULL,
+	[PaymentDate] DATETIME2,
+	[PaymentAmount] DECIMAL(18,2),
+	[StudentID] INT FOREIGN KEY REFERENCES [Students]([StudentID])
+)
+
+CREATE TABLE [Subjects](
+	[SubjectID] INT PRIMARY KEY NOT NULL,
+	[SubjectName] VARCHAR(64) NOT NULL
+)
+
+CREATE TABLE [Agenda](
+	[StudentID] INT FOREIGN KEY REFERENCES [Students]([StudentID]),
+	[SubjectID] INT FOREIGN KEY REFERENCES [Subjects]([SubjectID]),
+	CONSTRAINT [PK_Agenda] PRIMARY KEY ([StudentID], [SubjectID])
+)
+
