63658a228ce1dfa592ad42eca8b23ed945be2195
Ivaylo Minchev <159180386+ivaylominchev@users.noreply.github.com>
2025-01-29 13:37:25 +0200

    Update CRUD.sql

--- CRUD.sql
+++ CRUD.sql
@@ -118,3 +118,14 @@ SELECT TOP(7) [FirstName],
          FROM [Employees]
 	 ORDER BY [HireDate] DESC
 
+--Task 21
+UPDATE [Employees]
+   SET [Salary] *= 1.12
+ WHERE [DepartmentID] IN (
+							SELECT [DepartmentID]
+						      FROM [Departments]
+                             WHERE [Name] IN ('Engineering', 'Tool Design', 'Marketing', 'Information Services')
+                        )
+SELECT [Salary]
+  FROM [Employees]
+
