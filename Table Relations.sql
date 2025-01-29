18c08bf2da2e39b9f4f076877a88b2075fd99b65
Ivaylo Minchev <159180386+ivaylominchev@users.noreply.github.com>
2025-01-30 00:57:19 +0200

    Update Table Relations.sql

--- Table Relations.sql
+++ Table Relations.sql
@@ -187,3 +187,12 @@ CREATE TABLE [Agenda](
 	CONSTRAINT [PK_Agenda] PRIMARY KEY ([StudentID], [SubjectID])
 )
 
+
+--Task 9
+USE [Geography]
+
+  SELECT M.[MountainRange], P.[PeakName], P.[Elevation]
+    FROM [Mountains] AS M JOIN [Peaks] AS P
+	  ON P.MountainId = M.Id
+   WHERE M.[MountainRange] = 'Rila'
+ORDER BY P.[Elevation] DESC
