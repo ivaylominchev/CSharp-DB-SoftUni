472919b57f8a258318e0d0ea6d07522cbff61c70
Ivaylo Minchev <159180386+ivaylominchev@users.noreply.github.com>
2025-01-31 15:37:42 +0200

    Update SQL - Built-in Functions.sql

--- SQL - Built-in Functions.sql
+++ SQL - Built-in Functions.sql
@@ -142,3 +142,25 @@ ORDER BY [Email Provider] ASC,
    WHERE PATINDEX('___.1%.%.___', [IpAddress]) = 1
 ORDER BY [Username] ASC
 
+--Task 17
+  SELECT [g].[Name]
+      AS [Game],
+	     CASE
+              WHEN DATEPART(HOUR, [g].[Start]) BETWEEN 0 AND 11 THEN 'Morning'
+              WHEN DATEPART(HOUR, [g].[Start]) BETWEEN 12 AND 17 THEN 'Afternoon'
+              ELSE 'Evening'
+          END
+      AS [Part of the Day],
+         CASE
+              WHEN [g].[Duration] <= 3 THEN 'Extra Short'
+              WHEN [g].[Duration] BETWEEN 4 AND 6 THEN 'Short'
+              WHEN [g].[Duration] > 6 THEN 'Long'
+              ELSE 'Extra Long'
+          END
+      AS [Duration]
+    FROM [Games]
+      AS [g]
+ORDER BY [Game] ASC,
+         [Duration] ASC,
+         [Part of the Day] ASC
+
