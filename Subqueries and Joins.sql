fefc374d588fcb6189b3c97bc78257694e9de2f1
Ivaylo Minchev <159180386+ivaylominchev@users.noreply.github.com>
2025-02-01 22:32:46 +0200

    Update Subqueries and Joins.sql

--- Subqueries and Joins.sql
+++ Subqueries and Joins.sql
@@ -37,3 +37,17 @@ INNER JOIN [Towns]
   ORDER BY [e].[FirstName] ASC,
            [e].[LastName] ASC
 
+--Problem 03
+    SELECT [e].[EmployeeID],
+           [e].[FirstName],
+           [e].[LastName],
+           [d].[Name]
+        AS [DepartmentName]
+      FROM [Employees]
+        AS [e]
+INNER JOIN [Departments]
+        AS [d]
+        ON [e].[DepartmentID] = [d].[DepartmentID]
+     WHERE [d].[Name] = 'Sales'
+  ORDER BY [e].[EmployeeID] ASC
+
