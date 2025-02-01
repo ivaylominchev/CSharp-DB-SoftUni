a041a501f959b84124a1580d89cbce08a916ee89
Ivaylo Minchev <159180386+ivaylominchev@users.noreply.github.com>
2025-02-01 23:09:06 +0200

    Update Subqueries and Joins.sql

--- Subqueries and Joins.sql
+++ Subqueries and Joins.sql
@@ -80,3 +80,17 @@ LEFT JOIN [EmployeesProjects]
     WHERE [ep].[ProjectID] IS NULL
  ORDER BY [e].[EmployeeID] ASC
 
+--Problem 06
+    SELECT [e].[FirstName],
+           [e].[LastName],
+           [e].[HireDate],
+           [d].[Name]
+        AS [DeptName]
+      FROM [Employees]
+        AS [e]
+INNER JOIN [Departments]
+        AS [d]
+        ON [e].[DepartmentID] = [d].[DepartmentID]
+     WHERE [e].[HireDate] > '1999-01-01' AND [d].[Name] IN ('Sales', 'Finance')
+  ORDER BY [e].[HireDate] ASC
+
