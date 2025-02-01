d7d739ec606335567a53e0b8702e7726754798a4
Ivaylo Minchev <159180386+ivaylominchev@users.noreply.github.com>
2025-02-01 22:24:57 +0200

    Update Subqueries and Joins.sql

--- Subqueries and Joins.sql
+++ Subqueries and Joins.sql
@@ -18,3 +18,22 @@ INNER JOIN [Addresses]
         ON [e].[AddressID] = [a].[AddressID]
   ORDER BY [e].[AddressID] ASC
 
+--Problem 02
+    SELECT 
+       TOP (50)
+           [e].[FirstName],
+           [e].[LastName],
+           [t].[Name]
+        AS [Town],
+           [a].[AddressText]
+      FROM [Employees]
+        AS [e]
+INNER JOIN [Addresses]
+        AS [a] 
+        ON [e].[AddressID] = [a].[AddressID]
+INNER JOIN [Towns]
+        AS [t]
+        ON [a].[TownID] = [t].[TownID]
+  ORDER BY [e].[FirstName] ASC,
+           [e].[LastName] ASC
+
