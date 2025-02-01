976582836fd3d978d41fbe47d4c27dc63e187ab3
Ivaylo Minchev <159180386+ivaylominchev@users.noreply.github.com>
2025-02-02 00:10:15 +0200

    Update Subqueries and Joins.sql

--- Subqueries and Joins.sql
+++ Subqueries and Joins.sql
@@ -131,3 +131,16 @@ INNER JOIN [Projects]
         ON [ep].[ProjectID] = [p].[ProjectID]
      WHERE [ep].[EmployeeID] = 24
 
+--Problem 09
+   SELECT [e1].[EmployeeID],
+          [e1].[FirstName],
+          [e1].[ManagerID],
+          [e2].[FirstName]
+     FROM [Employees]
+       AS [e1]
+LEFT JOIN [Employees]
+       AS [e2]
+       ON [e1].[ManagerID] = [e2].[EmployeeID]
+    WHERE [e1].[ManagerID] IN (3, 7)
+ ORDER BY [e1].[EmployeeID] ASC
+
