3ca833a41b228722c1c8b35c8369aba4d8d01751
Ivaylo Minchev <159180386+ivaylominchev@users.noreply.github.com>
2025-02-02 15:50:26 +0200

    Update Subqueries and Joins.sql

--- Subqueries and Joins.sql
+++ Subqueries and Joins.sql
@@ -177,3 +177,20 @@ SELECT MIN([a].[AverageSalary])
        )
     AS [a]
 
+USE [Geography]
+--Problem 12
+   SELECT [mc].[CountryCode],
+          [m].[MountainRange],
+          [p].[PeakName],
+          [p].[Elevation]
+     FROM [MountainsCountries]
+       AS [mc]
+LEFT JOIN [Mountains]
+       AS [m]
+       ON [mc].[MountainId] = [m].[Id]
+LEFT JOIN [Peaks]
+       AS [p]
+       ON [m].[Id] = [p].[MountainId]
+    WHERE [mc].[CountryCode] = 'BG' AND [p].[Elevation] > 2835
+ ORDER BY [p].[Elevation] DESC
+
