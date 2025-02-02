cd5f97d67e2eabb295057c525c132ea303e15c65
Ivaylo Minchev <159180386+ivaylominchev@users.noreply.github.com>
2025-02-02 12:45:29 +0200

    Update Subqueries and Joins.sql

--- Subqueries and Joins.sql
+++ Subqueries and Joins.sql
@@ -144,3 +144,23 @@ LEFT JOIN [Employees]
     WHERE [e1].[ManagerID] IN (3, 7)
  ORDER BY [e1].[EmployeeID] ASC
 
+ --Problem 10
+   SELECT
+      TOP (50)
+          [e1].[EmployeeID],
+          CONCAT_WS (' ', [e1].[FirstName], [e1].[LastName])
+	   AS [EmployeeName],
+          CONCAT_WS (' ', [e2].[FirstName], [e2].[LastName])
+       AS [ManagerName],
+          [d].[Name]
+       AS [DepartmentName]
+     FROM [Employees]
+       AS [e1]
+LEFT JOIN [Employees]
+       AS [e2]
+       ON [e1].[ManagerID] = [e2].[EmployeeID]
+LEFT JOIN [Departments]
+       AS [d]
+       ON [e1].[DepartmentID] = [d].[DepartmentID]
+ ORDER BY [e1].[EmployeeID] ASC
+
