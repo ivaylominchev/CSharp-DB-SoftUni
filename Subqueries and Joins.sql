07d963f4488aff952c31a8dc4d00189f9ace3417
Ivaylo Minchev <159180386+ivaylominchev@users.noreply.github.com>
2025-02-02 16:24:47 +0200

    Update Subqueries and Joins.sql

--- Subqueries and Joins.sql
+++ Subqueries and Joins.sql
@@ -205,3 +205,19 @@ LEFT JOIN [MountainsCountries]
        ON [mc].[CountryCode] = [c].[CountryCode]
     WHERE [c].[CountryCode] IN ('US', 'RU', 'BG')
  GROUP BY [c].[CountryCode]
+
+ --Problem 14
+   SELECT 
+      TOP (5)
+          [c].[CountryName],
+          [r].[RiverName]
+     FROM [Countries]
+       AS [c]
+LEFT JOIN [CountriesRivers]
+       AS [cr]
+       ON [c].[CountryCode] = [cr].[CountryCode]
+LEFT JOIN [Rivers]
+       AS [r]
+       ON [cr].[RiverId] = [r].[Id]
+    WHERE [c].[ContinentCode] = 'AF'
+ ORDER BY [c].[CountryName] ASC
