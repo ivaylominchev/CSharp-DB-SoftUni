ff9182f67d805501660bf44ed8ef45b32feae8eb
Ivaylo Minchev <159180386+ivaylominchev@users.noreply.github.com>
2025-02-03 22:39:24 +0200

    Update Indices and Data Aggregation.sql

--- Indices and Data Aggregation.sql
+++ Indices and Data Aggregation.sql
@@ -54,3 +54,14 @@ GROUP BY [DepositGroup]
   HAVING SUM([DepositAmount]) < 150000
 ORDER BY [TotalSum] DESC
 
+--Problem 08
+  SELECT [DepositGroup],
+         [MagicWandCreator],
+         MIN([DepositCharge])
+      AS [MinDepositCharge]
+    FROM [WizzardDeposits]
+GROUP BY [DepositGroup],
+         [MagicWandCreator]
+ORDER BY [MagicWandCreator] ASC,
+         [DepositGroup] ASC
+
