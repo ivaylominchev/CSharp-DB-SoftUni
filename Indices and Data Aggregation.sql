d9dae4e7b3ad6b08d650f15523d8ecde451f6a31
Ivaylo Minchev <159180386+ivaylominchev@users.noreply.github.com>
2025-02-04 13:02:46 +0200

    Update Indices and Data Aggregation.sql

--- Indices and Data Aggregation.sql
+++ Indices and Data Aggregation.sql
@@ -168,3 +168,23 @@ ORDER BY [DepartmentID] ASC
      AND [HireDate] > '01/01/2000'
 GROUP BY [DepartmentID]
 
+--Problem 15
+SELECT *
+  INTO [EmployeesWithHighSalary]
+  FROM [Employees]
+ WHERE [Salary] > 30000
+
+DELETE
+  FROM [EmployeesWithHighSalary]
+ WHERE [ManagerID] = 42
+
+UPDATE [EmployeesWithHighSalary]
+   SET [Salary] += 5000
+ WHERE [DepartmentID] = 1
+ 
+  SELECT [DepartmentID],
+         AVG([Salary])
+      AS [AverageSalary]
+    FROM [EmployeesWithHighSalary]
+GROUP BY [DepartmentID]
+
