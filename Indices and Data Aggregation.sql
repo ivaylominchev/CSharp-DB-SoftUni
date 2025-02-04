e720230b4ad0cea8e28c009c65fbb59867547392
Ivaylo Minchev <159180386+ivaylominchev@users.noreply.github.com>
2025-02-04 14:07:35 +0200

    Update Indices and Data Aggregation.sql

--- Indices and Data Aggregation.sql
+++ Indices and Data Aggregation.sql
@@ -218,3 +218,19 @@ SELECT COUNT(*)
 GROUP BY [DepartmentID],
          [Salary]
 
+--Problem 19
+  SELECT 
+     TOP (10)
+         [e1].[FirstName],
+         [e1].[LastName],
+         [e1].[DepartmentID]
+    FROM [Employees]
+      AS [e1]
+   WHERE [e1].[Salary] > (
+                           SELECT AVG([e2].[Salary])
+                               AS [AvgSalary]
+                             FROM [Employees]
+                               AS [e2]
+                            WHERE [e2].[DepartmentID] = [e1].[DepartmentID]
+                         )
+ORDER BY [e1].[DepartmentID] ASC
