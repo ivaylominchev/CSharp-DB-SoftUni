45c696c5e483e7118f3c6cd497c875c224a3020d
Ivaylo Minchev <159180386+ivaylominchev@users.noreply.github.com>
2025-02-04 12:46:57 +0200

    Update Indices and Data Aggregation.sql

--- Indices and Data Aggregation.sql
+++ Indices and Data Aggregation.sql
@@ -159,3 +159,12 @@ GO
 GROUP BY [DepartmentID]
 ORDER BY [DepartmentID] ASC
 
+--Problem 14
+  SELECT [DepartmentID],
+         MIN([Salary])
+      AS [MinimumSalary]
+    FROM [Employees]
+   WHERE [DepartmentID] IN (2, 5, 7) 
+     AND [HireDate] > '01/01/2000'
+GROUP BY [DepartmentID]
+
