5b0945ce87d2226ca816e73d13ea630b13819c2c
Ivaylo Minchev <159180386+ivaylominchev@users.noreply.github.com>
2025-02-04 11:41:12 +0200

    Update Indices and Data Aggregation.sql

--- Indices and Data Aggregation.sql
+++ Indices and Data Aggregation.sql
@@ -106,3 +106,24 @@ GROUP BY [DepositGroup],
 ORDER BY [DepositGroup] DESC,
          [IsDepositExpired] ASC
 
+--Problem 12
+SELECT SUM([Difference])
+    AS [SumDifference]
+  FROM (
+            SELECT [wd1].[FirstName]
+                AS [Host Wizard],
+                   [wd1].[DepositAmount]
+                AS [Host Wizard Deposit],
+                   [wd2].[FirstName]
+                AS [Guest Wizard],
+                   [wd2].[DepositAmount]
+                AS [Guest Wizard Deposit],
+                   [wd1].[DepositAmount] - [wd2].[DepositAmount]
+                AS [Difference]
+              FROM [WizzardDeposits]
+                AS [wd1]
+        INNER JOIN [WizzardDeposits]
+                AS [wd2]
+                ON [wd1].[Id] + 1 = [wd2].[Id]
+       )
+    AS [HostGuestWizardTempTable]
