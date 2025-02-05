e12ab9137b23112575d2c063b95d5e5daacc7792
Ivaylo Minchev <159180386+ivaylominchev@users.noreply.github.com>
2025-02-05 16:31:26 +0200

    Update Functions and Stored Procedures.sql

--- Functions and Stored Procedures.sql
+++ Functions and Stored Procedures.sql
@@ -118,3 +118,35 @@ GO
 EXEC [dbo].[usp_EmployeesBySalaryLevel] 'High'
 
 GO
+
+--Problem 07
+  CREATE
+      OR
+   ALTER
+FUNCTION [dbo].[ufn_IsWordComprised](@setOfLetters VARCHAR(100), @word VARCHAR(70))
+ RETURNS
+     BIT
+      AS
+   BEGIN
+           DECLARE @wordIndex TINYINT = 1;
+           WHILE(@wordIndex <= LEN(@word))
+           BEGIN
+                 DECLARE @currentLetter CHAR(1);
+                 SET @currentLetter = SUBSTRING(@word, @wordIndex, 1);
+
+                 IF(CHARINDEX(@currentLetter, @setOfLetters) = 0)
+                 BEGIN
+                       RETURN 0;
+                 END
+
+                 SET @wordIndex += 1;
+           END
+
+           RETURN 1
+     END
+
+GO
+
+SELECT [dbo].[ufn_IsWordComprised]('oistmiahf', 'Sofia')
+
+GO
