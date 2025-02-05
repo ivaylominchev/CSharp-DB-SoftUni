c229b70cc37b4df3d5f4641b9dd29140b82fc2d9
Ivaylo Minchev <159180386+ivaylominchev@users.noreply.github.com>
2025-02-05 17:18:20 +0200

    Update Functions and Stored Procedures.sql

--- Functions and Stored Procedures.sql
+++ Functions and Stored Procedures.sql
@@ -183,6 +183,10 @@ CREATE OR ALTER PROCEDURE [dbo].[usp_DeleteEmployeesFromDepartment] @departmentI
          DELETE
            FROM [Employees]
           WHERE [DepartmentID] = @departmentId
+		  
+         DELETE
+           FROM [Departments]
+          WHERE [DepartmentID] = @departmentId
 
          SELECT COUNT(*)
            FROM [Employees]
