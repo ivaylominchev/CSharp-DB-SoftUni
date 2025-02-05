c2cb1985dad79994d7c0b636a85b208eeeef56d0
Ivaylo Minchev <159180386+ivaylominchev@users.noreply.github.com>
2025-02-05 17:15:06 +0200

    Update Functions and Stored Procedures.sql

--- Functions and Stored Procedures.sql
+++ Functions and Stored Procedures.sql
@@ -150,3 +150,43 @@ GO
 SELECT [dbo].[ufn_IsWordComprised]('oistmiahf', 'Sofia')
 
 GO
+
+--Problem 08
+CREATE OR ALTER PROCEDURE [dbo].[usp_DeleteEmployeesFromDepartment] @departmentId INT
+    AS
+ BEGIN
+         DELETE
+           FROM [EmployeesProjects]
+          WHERE [EmployeeID] IN (
+                                 SELECT [EmployeeID]
+                                   FROM [Employees]
+                                  WHERE [DepartmentID] = @departmentId
+                                )
+         UPDATE [Employees]
+            SET [ManagerID] = NULL
+          WHERE [ManagerID] IN (
+                                SELECT [EmployeeID]
+                                  FROM [Employees]
+                                 WHERE [DepartmentID] = @departmentId
+                               )
+
+         ALTER TABLE [Departments]
+         ALTER COLUMN [ManagerID] INT NULL
+
+         UPDATE [Departments]
+            SET [ManagerID] = NULL
+          WHERE [ManagerID] IN (
+                                SELECT [EmployeeID]
+                                  FROM [Employees]
+                                 WHERE [DepartmentID] = @departmentId   
+                              )
+         DELETE
+           FROM [Employees]
+          WHERE [DepartmentID] = @departmentId
+
+         SELECT COUNT(*)
+           FROM [Employees]
+          WHERE [DepartmentID] = @departmentId
+  END
+
+GO
