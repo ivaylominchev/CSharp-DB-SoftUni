d1d0fad0e499392e8ce2d19763fe87e62d958fbd
Ivaylo Minchev <159180386+ivaylominchev@users.noreply.github.com>
2025-02-08 14:23:43 +0200

    Update MS SQL Exam Preparation 1.sql

--- MS SQL Exam Preparation 1.sql
+++ MS SQL Exam Preparation 1.sql
@@ -257,3 +257,28 @@ GO
 SELECT dbo.udf_AuthorsWithBooks('J.K. Rowling')
 
 GO
+
+--Problem 12
+CREATE PROCEDURE [dbo].[usp_SearchByGenre](@genreName NVARCHAR(30))
+AS 
+ BEGIN
+         SELECT [b].[Title],
+                [b].[YearPublished]
+             AS [Year],
+                [b].[ISBN],
+                [a].[Name] 
+             AS [Author],
+                [g].[Name]
+             AS [Genre]
+           FROM [Books]
+             AS [b]
+     INNER JOIN [Authors]
+             AS [a]
+             ON [b].[AuthorId] = [a].[Id]
+     INNER JOIN [Genres]
+             AS [g]
+             ON [b].[GenreId] = [g].[Id]
+          WHERE [g].[Name] = @genreName
+       ORDER BY [b].[Title] ASC
+ END
+
