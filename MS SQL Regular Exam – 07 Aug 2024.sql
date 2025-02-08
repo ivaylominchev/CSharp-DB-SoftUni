880cdabc9397e71eecaefdbbe781d4024848cd9b
Ivaylo Minchev <159180386+ivaylominchev@users.noreply.github.com>
2025-02-08 20:21:18 +0200

    Update MS SQL Regular Exam – 07 Aug 2024.sql

--- MS SQL Regular Exam – 07 Aug 2024.sql
+++ MS SQL Regular Exam – 07 Aug 2024.sql
@@ -99,3 +99,24 @@ UPDATE [Shoes]
                         WHERE [Name] = 'Nike'
                     )
 
+--Problem 04
+DELETE
+  FROM [Orders]
+ WHERE [ShoeId] IN (
+                     SELECT [Id]
+                       FROM [Shoes]
+                      WHERE [Model] = 'Joyride Run Flyknit'
+				   )
+
+DELETE
+  FROM [ShoesSizes]
+ WHERE [ShoeId] IN (
+                     SELECT [Id]
+                       FROM [Shoes]
+                      WHERE [Model] = 'Joyride Run Flyknit'
+                   )
+
+DELETE
+  FROM [Shoes]
+ WHERE [Model] = 'Joyride Run Flyknit'
+
