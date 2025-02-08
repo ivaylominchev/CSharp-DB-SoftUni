2beaaffe5f3d3a4561fad65478253f690bfd5788
Ivaylo Minchev <159180386+ivaylominchev@users.noreply.github.com>
2025-02-08 20:08:28 +0200

    Update MS SQL Regular Exam – 07 Aug 2024.sql

--- MS SQL Regular Exam – 07 Aug 2024.sql
+++ MS SQL Regular Exam – 07 Aug 2024.sql
@@ -49,3 +49,44 @@ CREATE TABLE [ShoesSizes](
 	PRIMARY KEY ([ShoeId], [SizeId])
 )
 
+--Problem 02
+INSERT INTO [Brands]([Name])
+     VALUES ('Timberland'),
+            ('Birkenstock')
+
+INSERT INTO [Shoes]([Model], [Price], [BrandId])
+     VALUES ('Reaxion Pro', 150.00, 12),
+            ('Laurel Cort Lace-Up', 160.00, 12),
+            ('Perkins Row Sandal', 170.00, 12),
+            ('Arizona', 80.00, 13),
+            ('Ben Mid Dip', 85.00, 13),
+            ('Gizeh', 90.00, 13)
+
+INSERT INTO [ShoesSizes]([ShoeId], [SizeId])
+     VALUES (70, 1),
+            (70, 2),
+            (70, 3),
+            (71, 2),
+            (71, 3),
+            (71, 4),
+            (72, 4),
+            (72, 5),
+            (72, 6),
+			(73, 1),
+            (73, 3),
+            (73, 5),
+            (74, 2),
+            (74, 4),
+            (74, 6),
+            (75, 1),
+            (75, 2),
+            (75, 3)
+
+INSERT INTO [Orders]([ShoeId], [SizeId], [UserId])
+     VALUES (70, 2, 15),
+            (71, 3, 17),
+            (72, 6, 18),
+            (73, 5, 4),
+            (74, 4, 7),
+            (75, 1, 11)
+
