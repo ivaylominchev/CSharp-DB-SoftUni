d083359651dff10280c0e8bd33359cc1bed97edb
Ivaylo Minchev <159180386+ivaylominchev@users.noreply.github.com>
2025-02-09 13:24:53 +0200

    Update MS SQL Regular Exam - 9 February 2025.sql

--- MS SQL Regular Exam - 9 February 2025.sql
+++ MS SQL Regular Exam - 9 February 2025.sql
@@ -84,3 +84,191 @@ INSERT INTO [TeamStats]([TeamId], [Wins], [Draws], [Losses])
      VALUES (97, 15, 1, 3),
             (98, 14, 3, 2)
 
+--Problem 03
+    UPDATE [ps]
+       SET [ps].[Goals] += 1
+      FROM [PlayerStats] AS [ps]
+INNER JOIN [Players] AS [p]
+        ON [ps].[PlayerId] = [p].[Id]
+INNER JOIN [PlayersTeams] AS [pt]
+        ON [p].[Id] = [pt].[PlayerId]
+INNER JOIN [Teams] AS [t]
+        ON [t].[Id] = [pt].[TeamId]
+INNER JOIN [Leagues] AS [L]
+        ON [t].[LeagueId] = [L].[Id]
+     WHERE [p].[Position] = 'Forward' AND [L].[Name] = 'La Liga'
+
+--Problem 04
+DELETE FROM [PlayerStats]
+WHERE [PlayerId] IN (
+    SELECT P.[Id]
+    FROM [Players] P
+    JOIN [PlayersTeams] PT ON PT.[PlayerId] = P.[Id]
+    JOIN [Teams] T ON PT.[TeamId] = T.[Id]
+    JOIN [Leagues] L ON T.[LeagueId] = L.[Id]
+    WHERE L.[Name] = 'Eredivisie' AND P.[Name] IN ('Luuk de Jong', 'Josip Sutalo')
+);
+
+DELETE FROM [PlayersTeams]
+WHERE [PlayerId] IN (
+    SELECT P.[Id]
+    FROM [Players] P
+    JOIN [PlayersTeams] PT ON PT.[PlayerId] = P.[Id]
+    JOIN [Teams] T ON PT.[TeamId] = T.[Id]
+    JOIN [Leagues] L ON T.[LeagueId] = L.[Id]
+    WHERE L.[Name] = 'Eredivisie' AND P.[Name] IN ('Luuk de Jong', 'Josip Sutalo')
+);
+
+DELETE P
+FROM [Players] P
+JOIN [PlayersTeams] PT ON PT.[PlayerId] = P.[Id]
+JOIN [Teams] T ON PT.[TeamId] = T.[Id]
+JOIN [Leagues] L ON T.[LeagueId] = L.[Id]
+LEFT JOIN [PlayerStats] PS ON PS.[PlayerId] = P.[Id]
+WHERE L.[Name] = 'Eredivisie'
+AND P.[Name] IN ('Luuk de Jong', 'Josip Sutalo');
+
+
+--Problem 05
+SELECT FORMAT([MatchDate], 'yyyy-MM-dd')
+    AS [MatchDate],
+	   [HomeTeamGoals],
+       [AwayTeamGoals],
+	   [HomeTeamGoals] + [AwayTeamGoals]
+    AS [TotalGoals]
+  FROM [Matches]
+WHERE ([HomeTeamGoals] + [AwayTeamGoals]) >= 5
+ ORDER BY [TotalGoals] DESC,
+          [MatchDate] ASC
+
+--Problem 06
+    SELECT [p].[Name],
+           [t].[City]
+      FROM [Players] AS [p]
+INNER JOIN [PlayersTeams] AS [pt]
+        ON [p].[Id] = [pt].[PlayerId]
+INNER JOIN [Teams] AS [t]
+        ON [pt].[TeamId] = [t].[Id]
+     WHERE [p].[Name] LIKE '%Aaron%'
+  ORDER BY [p].[Name] ASC
+
+
+--Problem 07
+SELECT [p].[Id],
+       [p].[Name],
+       [p].[Position]
+  FROM [Players] AS [p]
+INNER JOIN [PlayersTeams] AS [pt]
+        ON [p].[Id] = [pt].[PlayerId]
+INNER JOIN [Teams] AS [t]
+        ON [pt].[TeamId] = [t].[Id]
+WHERE [t].[City] = 'London'
+ORDER BY [p].[Name] ASC
+
+--Problem 08
+    SELECT
+       TOP (10)
+           [t].[Name]
+        AS [HomeTeamName],
+           [t2].[Name]
+        AS [AwayTeamName],
+           [L].[Name]
+        AS [LeagueName],
+           FORMAT([m].[MatchDate], 'yyyy-MM-dd')
+        AS [MatchDate]
+      FROM [Matches]
+        AS [m]
+INNER JOIN [Teams]
+        AS [t]
+        ON [m].[HomeTeamId] = [t].[Id]
+INNER JOIN [Teams]
+        AS [t2]
+        ON [t2].[Id] = [m].[AwayTeamId]
+INNER JOIN [Leagues]
+        AS [L]
+        ON [L].[Id] = [t].[LeagueId]
+     WHERE [m].[MatchDate] BETWEEN '2024-09-01' AND '2024-09-15' AND [L].[Id] % 2 = 0
+  ORDER BY [m].[MatchDate] ASC,
+           [HomeTeamName] ASC
+
+--Problem 09
+    SELECT [m].[AwayTeamId]
+        AS [Id],
+           [t].[Name],
+		   SUM([m].[AwayTeamGoals])
+        AS [TotalAwayGoals]
+      FROM [Matches]
+        AS [m]
+INNER JOIN [Leagues]
+        AS [L]
+        ON [m].[LeagueId] = [L].[Id]
+INNER JOIN [Teams]
+      AS [t]
+      ON [m].[AwayTeamId] = [t].[Id]
+GROUP BY [m].[AwayTeamId],
+         [t].[Name]
+  HAVING SUM([m].[AwayTeamGoals]) >= 6
+ORDER BY [TotalAwayGoals] DESC,
+         [t].[Name] ASC
+
+--Problem 10
+    SELECT [L].[Name]
+        AS [LeagueName],
+           ROUND(AVG(CAST(([m].[HomeTeamGoals] + [m].[AwayTeamGoals]) AS FLOAT)), 2)
+        AS [AvgScoringRate]
+      FROM [Matches]
+        AS [m]
+INNER JOIN [Leagues]
+        AS [L]
+        ON [m].[LeagueId] = [L].[Id]
+  GROUP BY [L].[Name]
+  ORDER BY [AvgScoringRate] DESC
+
+GO
+
+--Problem 11
+CREATE FUNCTION [dbo].[udf_LeagueTopScorer](@nameLeague NVARCHAR(50))
+RETURNS TABLE AS
+ RETURN (
+			
+			SELECT [PlayerName],
+			       [TotalGoals]
+			  FROM  (SELECT 
+			       [p].[Name] AS [PlayerName],
+			       [ps].[Goals] AS [TotalGoals],
+			DENSE_RANK() OVER(PARTITION BY [L].[Id] ORDER BY [ps].[Goals] DESC)
+            AS [RANKS]
+			FROM [Players] AS [p]
+			INNER JOIN [PlayerStats] AS [ps]
+			ON [p].[Id] = [ps].[PlayerId]
+			INNER JOIN [PlayersTeams] AS [pt]
+			ON [p].[Id] = [pt].[PlayerId]
+			INNER JOIN [Teams] AS [t]
+			ON [pt].[TeamId] = [t].[Id]
+			INNER JOIN [Leagues] AS [L]
+			ON [L].[Id] = [t].[LeagueId]
+			WHERE [L].[Name] = @nameLeague
+			)
+		AS [RankingTempTable]
+			WHERE [RANKS] = '1'
+         )
+
+GO
+
+--Problem 12
+CREATE PROCEDURE [dbo].[usp_SearchTeamsByCity] @cityName NVARCHAR(50)
+AS 
+ BEGIN
+     SELECT [t].[Name] AS [TeamName],
+	        [L].[Name] AS [LeagueName],
+			[t].[City]
+	 FROM [Teams] AS [t]
+	 INNER JOIN [Leagues] AS [L]
+	 ON [t].[LeagueId] = [L].[Id]
+	 WHERE [t].[City] = @cityName
+	 ORDER BY [t].[Name] ASC
+ END
+
+GO 
+
+EXEC usp_SearchTeamsByCity 'London'
